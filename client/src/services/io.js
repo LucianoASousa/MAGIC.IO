7fdcf4f6f96400c94a94c3883fb18e800ad1da08
Luciano <lucianoamorim506@gmail.com>
2023-01-15 22:38:16 -0300

    settings

--- client/src/services/io.js
+++ client/src/services/io.js
@@ -1,2 +1,4 @@
 import io from "socket.io-client"
-export const socket = io.connect(import.meta.env.VITE_URL_MAGIC);
+import dotenv from "dotenv"
+dotenv.config()
+export const socket = io.connect(process.env.VITE_URL_MAGIC);
